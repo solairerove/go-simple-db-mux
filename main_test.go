1a3456807a2243460faa315b97b4a9f58fb65480
solairerove <hax2033@gmail.com>
2018-09-12 16:39:35 +0300

    feature: write integaration tests as tdd methodology)

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
 	"."
@@ -95,6 +96,77 @@ func TestCreateProduct(t *testing.T) {
 
 	if m["id"] != 1.0 {
 		t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+	}
+}
+
+func TestGetProduct(t *testing.T) {
+	clearTable()
+
+	addProduct(1)
+
+	req, _ := http.NewRequest("GET", "/products/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func TestUpdateProduct(t *testing.T) {
+	clearTable()
+	addProduct(1)
+
+	req, _ := http.NewRequest("GET", "/products/1", nil)
+	response := executeRequest(req)
+	var originalProduct map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalProduct)
+
+	payload := []byte(`{"name":"test product - updated name", "price":11.22}`)
+
+	req, _ = http.NewRequest("PUT", "/products/1", bytes.NewBuffer(payload))
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != originalProduct["name"] {
+		t.Errorf("Expected the name to remain the same (%v). Got %v", originalProduct["name"], m["name"])
+	}
+
+	if m["price"] != originalProduct["price"] {
+		t.Errorf("Expected the price to remain the same (%v). Got %v", originalProduct["price"], m["price"])
+	}
+
+	if m["id"] != originalProduct["id"] {
+		t.Errorf("Expected the id to remain the same (%v). Got %v", originalProduct["id"], m["id"])
+	}
+}
+
+func TestDeleteProduct(t *testing.T) {
+	clearTable()
+	addProduct(1)
+
+	req, _ := http.NewRequest("GET", "/products/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/products/1", nil)
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/products/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+}
+
+func addProduct(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO products(name, price) VALUES ($1, $2)", "Product "+strconv.Itoa(i), (i+1.0)*10)
 	}
 }
 
