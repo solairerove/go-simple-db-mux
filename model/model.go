ba63cc7c1503a101e9cceb78accac4ea96c2446a
solairerove <hax2033@gmail.com>
2018-09-15 13:48:39 +0300

    feature: add tbd comments

--- model/model.go
+++ model/model.go
@@ -4,12 +4,14 @@ import (
 	"database/sql"
 )
 
+// Product ... tbd
 type Product struct {
 	ID    int     `json:"id"`
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
 
+// CreateProduct ... tbd
 func (p *Product) CreateProduct(db *sql.DB) error {
 	err := db.QueryRow("INSERT INTO products(name, price) VALUES ($1, $2) RETURNING id", p.Name, p.Price).Scan(&p.ID)
 
@@ -20,10 +22,12 @@ func (p *Product) CreateProduct(db *sql.DB) error {
 	return nil
 }
 
+// GetProduct ... tbd
 func (p *Product) GetProduct(db *sql.DB) error {
 	return db.QueryRow("SELECT name, price FROM products WHERE id=$1", p.ID).Scan(&p.Name, &p.Price)
 }
 
+// GetProducts ... tbd
 func GetProducts(db *sql.DB, start, count int) ([]Product, error) {
 	rows, err := db.Query(
 		"SELECT id, name, price FROM products LIMIT $1 OFFSET $2",
@@ -48,6 +52,7 @@ func GetProducts(db *sql.DB, start, count int) ([]Product, error) {
 	return products, nil
 }
 
+// UpdateProduct ... tbd
 func (p *Product) UpdateProduct(db *sql.DB) error {
 	_, err :=
 		db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3",
@@ -56,6 +61,7 @@ func (p *Product) UpdateProduct(db *sql.DB) error {
 	return err
 }
 
+// DeleteProduct ... tbd
 func (p *Product) DeleteProduct(db *sql.DB) error {
 	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
 
